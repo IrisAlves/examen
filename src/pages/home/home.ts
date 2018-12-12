a7c38c9cf3c2dbfd2c31d9923c2b48c033810b23
unknown <dam201@ACARBALLEIRA.LOCAL>
2018-12-12 14:59:01 +0100

    fin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';/*
+import { Toast } from '@ionic-native/toast';*/
+import { Pagina2Page } from '../pagina2/pagina2';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  pag2 = Pagina2Page;
+  constructor(public navCtrl: NavController, /*private toast: Toast*/) {
 
   }
+  
+  num=0;
+  nconta : string = "..";
+  incrementarnum(){
+    this.num=this.num+1;
+  }
+  
+  Irapagina2(){
+    this.navCtrl.push("Pagina2Page",this.num);
+  }
 
+  clicmensaje(){
+    /*
+  this.toast.show(`Iris alves`, '3000', 'center').subscribe(
+    toast => {
+      console.log(toast);
+    }
+  );*/
+  }
 }
